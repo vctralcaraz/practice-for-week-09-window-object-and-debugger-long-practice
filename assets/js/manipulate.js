370a3c09eb98ceed8f3657fa57ab038e856270ec
Victor Alcaraz <vctralcaraz@gmail.com>
2024-01-30 17:50:36 -0800

    done with no bonus

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,21 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
     // Your code here
+    window.document.title = "Victor's Portfolio";
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
     // Your code here
+    window.document.body.children[0].children[0].innerHTML = "Victor Alcaraz";
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
 
     // Your code here
+    window.document.body.children[1].children[1].innerHTML = "In 2015, Victor started his journey to becoming a CS major at RCC. He did obtain his C++ certification in 2017.";
 }
